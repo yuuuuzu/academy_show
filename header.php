b60810b173478a269257a613f19158806c7b23a4
yuzu <naiyangyuantian609@gmail.com>
2024-03-04 14:13:14 +0900

    [Add] header

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>アカデミー翔</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet" href="css/h_style.css">
+</head>
+
+<body>
+    <header id="top">
+        <h1>アカデミー翔</h1>
+        <nav id="nav-li">
+            <ul>
+                <li><a href="movie.php">動画一覧</a></li>
+                <li><a href="index.php">ログイン</a></li>
+                <li><a href="contact.php">お問い合わせ</a></li>
+            </ul>
+        </nav>
+    </header>
